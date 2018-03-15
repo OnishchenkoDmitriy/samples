7a0ed6cc99a65723351abd8b8f27639a1bc10f5d
OnyshchenkoDmytro <faoksss@outlook.com>
2018-03-15 16:32:25 +0200

    adde depends-on tag in t1000_context.xml

---
+++ springSamples/Lesson5/src/main/java/org/training/impl/DataBase.java
@@ -0,0 +1,12 @@
+package org.training.impl;
+
+public class DataBase {
+
+    public DataBase() {
+        testConnection();
+    }
+
+    private void testConnection(){
+        System.out.println("Test connection!");
+    }
+}
