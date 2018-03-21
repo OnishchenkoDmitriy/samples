ecdec6a64bdd2afd28088403fafa067350f83ccd
OnyshchenkoDmytro <faoksss@outlook.com>
2018-03-21 17:16:57 +0200

    added SpeakerService and SpeakerRepo

---
+++ springSamples/ConferenceManager/src/main/java/org/training/repository/impl/InMemorySpeakerRepo.java
@@ -0,0 +1,6 @@
+package org.training.repository.impl;
+
+import org.training.repository.SpeakerRepo;
+
+public class InMemorySpeakerRepo implements SpeakerRepo {
+}
---
+++ springSamples/ConferenceManager/src/main/java/org/training/service/impl/SimpleSpeakerService.java
@@ -0,0 +1,6 @@
+package org.training.service.impl;
+
+import org.training.service.SpeakerService;
+
+public class SimpleSpeakerService implements SpeakerService {
+}
