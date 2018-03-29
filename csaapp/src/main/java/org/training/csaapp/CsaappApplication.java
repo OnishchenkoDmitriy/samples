8da803a7bc7a9cfd6b181609b121b6241a9e0503
Onyshchenko Dmytro <faoksss@outlook.com>
2018-03-29 23:57:59 +0300

    added new project csaapp

---
+++ csaapp/src/main/java/org/training/csaapp/CsaappApplication.java
@@ -0,0 +1,12 @@
+package org.training.csaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsaappApplication.class, args);
+	}
+}
